d747e3c25356ef945ea5f9369d00851de95cc4dc
MGVera <madelinegvera@gmail.com>
2017-01-05 18:45:55 -0500

    registering shows up but doesn't work

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,35 @@
-angular
-  .module('thinkster', []);
+(function(){
+	'use-strict';
+
+	angular
+		.module('thinkster', [
+			'thinkster.routes',
+			'thinkster.config',
+			'thinkster.authentication'
+			]);
+
+	angular
+		.module('thinkster.routes', ['ngRoute']);
+
+	angular
+		.module('thinkster.config', []);
+
+	angular
+		.module('thinkster')
+		.run(run);
+
+	run.$inject = ['$http'];
+
+	function run($http){
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+	//Header expected by Django
+    $http.defaults.xsrfCookieName = 'csrftoken'; 
+
+}
+
+ 
+})();
+
+
+
